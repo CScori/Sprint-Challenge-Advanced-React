81ccfa27c7dc4d35c38147c3ea5404d01841aac3
CScori <amilanotte1366@gmail.com>
2019-10-04 10:43:26 -0500

    adds follow map from data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import axios from 'axios'
+import Players from "./components/Players"
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/players')
+      .then(res => {
+        this.setState({
+          players: res.data
+        });
+        console.log('res', res.data)
+      })
+      .catch(err => console.log(err));
+  }
 
 
- 
   render() {
     return (
-      <div>TEST</div>
+      <div>
+        {this.state.players.map(player => (
+          <div>
+            <Players player={player}/>
+          </div>
+        ))}
+        
+      </div>
     )
   }
 }
---
+++ client/src/components/Players.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+const players = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default players
