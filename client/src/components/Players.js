eb2d65fda4b1bc788fc76c59a6b99d46ff6a7fac
CScori <amilanotte1366@gmail.com>
2019-10-04 11:34:54 -0500

    adds css and button to change mode

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Players = () => {
+    const [darkMode, setDarkMode] = useState()
+    const changeMode = e => {
+        e.preventDefault();
+        setDarkMode(!darkMode);
+    };
+
     return (
-        <div>
+        <div className="dark-mode__dark">
+            Choose Your Color
+            <button
+                onClick={changeMode}
+                className={darkMode ? 'dark darked' : 'dark'}
+            />
             
         </div>
     )
